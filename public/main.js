d950ac285773d41c7061831a1b7c090648ec9c9b
Alvin <alvin.farkhan@gmail.com>
2019-07-10 17:35:06 +0700

    dynamic events

--- public/main.js
+++ public/main.js
@@ -9,7 +9,75 @@ var database = firebase.database();
 					judul.push(berita.child("judul").val());
                 });
 				loadImg();
-            });
+});
+var event = [
+    {
+        'nama' : 'a',
+        'timestamp' : '1562626260',
+        'tempat' : 'x'
+    },
+    {
+        'nama' : 'b',
+        'timestamp' : '1562642260',
+        'tempat' : 'u'
+    },
+    {
+        'nama' : 'c',
+        'timestamp' : '1562625160',
+        'tempat' : 'z'
+    }
+]
+for (var i=0; i<3; i++) {
+    var date = new Date(event[i].timestamp*1000);
+    var bulan;
+    switch (date.getMonth()) {
+        case 0:
+            bulan = 'Januari'
+            break;
+        case 1:
+            bulan = 'Februari'
+            break;
+        case 2:
+            bulan = 'Maret'
+            break;
+        case 3:
+            bulan = 'April'
+            break;
+        case 4:
+            bulan = 'Mei'
+            break;
+        case 5:
+            bulan = 'Juni'
+            break;
+        case 6:
+            bulan = 'Juli'
+            break;
+        case 7:
+            bulan = 'Agustus'
+            break;
+        case 8:
+            bulan = 'September'
+            break;
+        case 9:
+            bulan = 'Oktober'
+            break;
+        case 10:
+            bulan = 'November'
+            break;
+        case 11:
+            bulan = 'Desember'
+            break;
+        default:
+            break;
+    }
+    event[i].tanggal = date.getDate() + ' ' + bulan + ' ' + date.getFullYear();
+    if (date.getHours()<10){event[i].waktu='0'}else{event[i].waktu=''};
+    event[i].waktu = event[i].waktu + date.getHours() + ':';
+    if (date.getMinutes()<10){event[i].waktu+='0'};
+    event[i].waktu = event[i].waktu + date.getMinutes();
+};
+
+var timestamp = 1562626260;
 
 // batas tommy
 
@@ -39,6 +107,7 @@ window.onscroll = function() {scrollFunction()};
 
 document.addEventListener('DOMContentLoaded', function() {
    loadImg();
+   loadEvent();
 });
 
 function nextCar(){
@@ -51,7 +120,7 @@ function nextCar(){
 }
 
 function loadImg(){
- var elems = document.querySelectorAll('.carousel');
+    var elems = document.querySelectorAll('.carousel');
     var instances = M.Carousel.init(elems, {});
     var elem = document.querySelectorAll('.sidenav');
     var instance = M.Sidenav.init(elem, {});
@@ -60,9 +129,22 @@ function loadImg(){
     for (var i=0; i<3; i++){
         carousels[i].style.backgroundImage = "url('"+ linkGambar[i] + "')";
     }
-
-    pollFunc(nextCar, 900000, 3000);	
+    pollFunc(nextCar, 900000, 5000);
 }
+
+function loadEvent(){
+    document.getElementById('mainCaptionEvents').innerHTML = event[0].nama;
+    document.getElementById('tanggalMainEvents').innerHTML = event[0].tanggal;
+    document.getElementById('jamMainEvents').innerHTML = event[0].waktu;
+    document.getElementById('lokasiMainEvents').innerHTML = event[0].tempat;
+    var tanggalSideEvents = document.querySelectorAll('.tanggalSideEvents');
+    tanggalSideEvents[0].innerHTML = event[1].tanggal;
+    tanggalSideEvents[1].innerHTML = event[2].tanggal;
+    var captionSideEvents = document.querySelectorAll('.captionSideEvents');
+    captionSideEvents[0].innerHTML = event[1].nama;
+    captionSideEvents[1].innerHTML = event[2].nama;
+}
+
 function prevCar(){
     var x = document.getElementById('carousel');
     var instance = M.Carousel.getInstance(x);
